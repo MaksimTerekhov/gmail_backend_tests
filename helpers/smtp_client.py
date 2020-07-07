d8a8ca200ef81675aaf6c350c82bf07df583cc7c
denvy47 <41b7a441b7a4>
2020-07-07 19:01:55 +0300

    added smtp_client.pyclient

---
+++ helpers/smtp_client.py
@@ -0,0 +1,23 @@
+from email.mime.base import MIMEBase
+from smtplib import SMTP_SSL
+from typing import List
+
+from helpers.endpoint_class import Endpoint
+from helpers.user_class import User
+
+
+class SmtpClient:
+    def __init__(self, endpoint: Endpoint, sender: User):
+        self.client = SMTP_SSL(endpoint.host, endpoint.port)
+        self.sender = sender
+
+    def login(self):
+        self.client.login(self.sender.email, self.sender.password)
+
+    def send(self, message: MIMEBase, sender: User, recipients: List[User]):
+        rcpts = [user.email for user in recipients]
+        self.client.send_message(message, sender.email, rcpts)
+
+    def close(self):
+        self.client.quit()
+        self.client.close()
