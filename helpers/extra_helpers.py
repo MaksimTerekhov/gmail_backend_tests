8af27b9dd131f1ff1c2b2c4825294dcf0ea48e57
denvy47 <41b7a441b7a4>
2020-07-12 16:49:06 +0300

    added waiter into extra_helpers.py

--- helpers/extra_helpers.py
+++ helpers/extra_helpers.py
@@ -1,7 +1,20 @@
 import random
 import string
+import time
 
 
 def random_ascii_string(length: int = 8):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+
+def waiter(func, *args, seconds: int = 10, **kwargs):
+    retry = 0
+
+    while retry < seconds:
+        result = func(*args, **kwargs)
+        if result is not None and not isinstance(result, Exception) and result != ('OK', [b'']):
+            return result
+
+        time.sleep(1)
+        retry += 1
