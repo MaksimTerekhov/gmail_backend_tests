29e19213096b873b1956533b19fb18754c613181
denvy47 <41b7a441b7a4>
2020-07-12 16:58:18 +0300

    added first test on correct mail working

--- tests/test_send_correct_message.py
+++ tests/test_send_correct_message.py
@@ -1,3 +1,4 @@
+from helpers.imap_client import MailboxFolders
 
 
 def test_send_correct_message(
@@ -5,3 +6,10 @@ def test_send_correct_message(
 ):
     message = test_context.message_helper.create_simple_text_message()
     test_context.message_helper.send(message)
+
+    actual_message = test_context.imap_helper.get_folder_content_by_params(
+        folder=MailboxFolders.INBOX,
+        params={'test-id': message['test-id']}
+    )
+
+    assert actual_message['Subject'] == 'test'
