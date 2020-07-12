3920784e940cdbb010e1586b6eda3445923610a4
denvy47 <41b7a441b7a4>
2020-07-12 15:37:49 +0300

    added method get_folder_content_by_params into imap client

--- helpers/imap_client.py
+++ helpers/imap_client.py
@@ -1,8 +1,19 @@
+import email
 from imaplib import IMAP4_SSL
 from socket import gaierror
+from typing import List, Dict
+
+from attr import dataclass
 
 from helpers.endpoint_class import Endpoint
 from helpers.user_class import User
+
+
+@dataclass
+class MailboxFolders:
+    INBOX = 'INBOX'
+    SENT = '"[Gmail]/Sent Mail"'
+    SPAM = '"[Gmail]/Spam"'
 
 
 class ImapClient:
@@ -21,6 +32,17 @@ class ImapClient:
 
     def login(self):
         self.client.login(self.user.email, self.user.password)
+
+    def _get_msg_id_by_params(self, params: Dict):
+        prms = ''.join([f'(HEADER {k} "{v}")' for k, v in params.items()])
+        return self.client.search(None, prms)[1][0]
+
+    def get_folder_content_by_params(self, folder: str, params: Dict):
+        self.client.select(folder)
+        msg_id = self._get_msg_id_by_params(params)
+
+        _, bytes_msg = self.client.fetch(msg_id, '(RFC822)')
+        return email.message_from_bytes(bytes_msg[0][1])
 
     def log_out(self):
         self.mailbox_cleanup()
